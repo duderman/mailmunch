6afb056aff64c9a6865fbf6db38d65c3df24a8ac
Nick Norkin <zduderman@gmail.com>
2025-10-13 10:49:30 +0100

    10k max tokens

--- lambda/weekly_report/main.go
+++ lambda/weekly_report/main.go
@@ -443,7 +443,7 @@ func generateAIReport(openaiAPIKey string, config *Config, currentWeek, previous
 					},
 				},
 			},
-			MaxCompletionTokens: openai.Int(5000),
+			MaxCompletionTokens: openai.Int(10000),
 		},
 	)
 
